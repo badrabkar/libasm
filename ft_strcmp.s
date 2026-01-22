0708084bc151cf024dfd09874844b4d077595065
badrabkar <doublequintal@gmail.com>
2026-01-22 12:17:28 +0100

    update: logic of checking the end of the string

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,20 +3,19 @@ global ft_strcmp
 section .text
 
 ft_strcmp:
-	xor ecx, ecx
-	xor eax, eax
+	xor ecx, ecx ; setting the counter to 0
+	xor eax, eax ; setting the accumulator rax to 0
 
 .loop:
 	mov al, [rdi + rcx]
-	cmp al, byte [rsi + rcx]
+	test al, byte [rsi + rcx] ; if one of the bytes equals to 0 ==> we reach the end of a string
+	jz .done
+	cmp al, byte [rsi + rcx] ; if the bytes are different we need to perform subtraction
 	jne .done
-	add ax, word [rsi + rcx]
-	test ax, ax
-	je .done
 	inc rcx
 	jmp .loop
-
+	
 .done:
-	movzx rdx, byte [rsi + rcx]
-	sub eax, edx 
+	movzx	rdx, byte [rsi + rcx]
+	sub		rax, rdx		
 	ret
