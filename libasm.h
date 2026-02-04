97ffa55334e56e47077b276cdf06b5a9269326f6
badrabkar <doublequintal@gmail.com>
2026-02-04 07:22:29 -0500

    feat: header file added

---
+++ libasm.h
@@ -0,0 +1,8 @@
+#ifndef LIBASM_H
+#define LIBASM_H
+
+size_t  strlen(const char *s);
+char    *strcpy(char *restrict dst, const char *restrict src);
+int     strcmp(const char *s1, const char *s2);
+
+#endif
