abf9def8be289ee7e85896e6276f1beb1350a105
badrabkar <doublequintal@gmail.com>
2026-07-26 12:02:54 +0100

    update: the logic of sub & increment of rdi, rsi

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,43 +1,22 @@
 
-		global	ft_strcmp
+        global	ft_strcmp
 
-		section	.text
+        section	.text
 
-ft_strcmp:	
-		xor	ecx, ecx
-.loop:		movzx	ax, byte [rdi + rcx]
-		movzx	dx, byte [rsi + rcx]
-		cmp	al, dl
-		jnz	.sub
-		test	al, dl
-		jz	.sub
-		inc	rcx
-		jmp	.loop
+ft_strcmp:
+.loop:
+        mov	al, byte [rdi]
+        mov	dl, byte [rsi]
+        cmp	al, dl
+        jnz	.sub
+        test	al, al
+        jz	.sub
+        inc rdi
+        inc rsi
+        jmp	.loop
 .sub:
-		sub	ax, dx 
-		cwde		; convert word in ax into double-word in eax  === movsx	eax, ax  
-		ret
+        movzx eax, al
+        movzx edx, dl
+        sub eax, edx
+        ret
 
-
-;global ft_strcmp
-;
-;section .text
-;
-;ft_strcmp:
-;	xor ecx, ecx
-;	xor eax, eax
-;
-;.loop:
-;	mov al, [rdi + rcx]
-;	cmp al, byte [rsi + rcx]
-;	jne .done
-;	add ax, word [rsi + rcx]
-;	test ax, ax
-;	je .done
-;	inc rcx
-;	jmp .loop
-;
-;.done:
-;	movzx rdx, byte [rsi + rcx]
-;	sub eax, edx 
-;	ret
