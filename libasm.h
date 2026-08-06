ba02ac1cd47d57f99f312666cdf7e8d6ed99c16a
badrabkar <doublequintal@gmail.com>
2026-08-06 11:41:06 +0100

    updated: added ft_read and ft_strdup prototypes

--- libasm.h
+++ libasm.h
@@ -6,5 +6,7 @@ extern	char	*ft_strcpy(char *dst, const char *src);
 extern	int	ft_strcmp(const char *s1, const char *s2);
 extern	int	ft_strcmp(const char *s1, const char *s2);
 extern	ssize_t ft_write(int fd, const void *buf, size_t count);
+extern	ssize_t ft_read(int fd, void *buf, size_t count);
+extern	char	*ft_strdup(const char *s);
 
 #endif
