71eb4f1ad3532063f2925ecf83f8c4acadbbb917
badrabkar <doublequintal@gmail.com>
2026-08-12 21:16:45 +0100

    feat: implementation of write syscall in x86_64 asm

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,20 +1,34 @@
+;
+; File: ft_write.s
+; Description: x86_64 assembly implementation of write() system call.
+;
+; Arguments:
+; rdi : int fd
+; rsi : 
+; rdx :
+
 
         global	ft_write
+        extern __errno_location
 
         section	.text
-        extern __errno_location
 
 ft_write:
         mov eax, 1
         syscall
+
         mov rcx, rax
         neg rcx
         cmp rax, 0
-        jl .done
-        ret
-.done:
+        jl .syscall_failure
+
+        jmp .exit
+
+.syscall_failure:
         call __errno_location WRT ..plt
         mov [rax], rcx
         mov rax, -1
+
+.exit:
         ret
 
